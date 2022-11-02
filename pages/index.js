dadbfa78dc349eab17e1dd6b9a4d5f4520605a20
Kaique Yamamoto | Full Stack Developer <kaique@redblock.com.br>
2022-11-01 22:49:29 -0300

    remove package-lock and move components folder

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import HeaderLinks  from '../components/Header'
-import NavBar from '../components/NavBar'
+import HeaderLinks  from './components/Header'
+import NavBar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
